128a891781261a40038c84f340c953a02d483bdc
Jinglong-Liu <1601029287@qq.com>
2021-11-12 21:00:17 +0800

    init a maven project

---
+++ game/src/test/java/com/game/AppTest.java
@@ -0,0 +1,28 @@
+package com.game;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    //extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    //public AppTest( String testName )
+    //{
+    //    super( testName );
+   // }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    @Test
+    public void test(){
+        assertTrue(true);
+    }
+}
